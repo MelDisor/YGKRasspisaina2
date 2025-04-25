da96d97eda674aa2cf8e7fa1a03da5bf074b838d
MelDisor <153506532+MelDisor@users.noreply.github.com>
2025-04-25 17:15:34 +0300

    Delete ягкhqd directory

--- ягкhqd/telegram.py
+++
@@ -1,8 +0,0 @@
-import telebot
-bot = telebot.TeleBot('6879959992:AAFrc0yiTNdes6edvHrtzqoC35jJWwSE0UU') 
-@bot.message_handler(commands=['start'])
-def main(message):
-    bot.send_message(message)
-    bot.send_message(message.chat.id, 'Привет выбери какая у тебя группа? ')
-
-    bot.polling (none_stop=True)
